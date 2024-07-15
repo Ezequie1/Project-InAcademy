22c85767284097580fd1083cd4243ae5e402f3b3
Ezequiel <ezequielalves1021@gmail.com>
2024-07-15 16:22:00 -0300

    Add conection to AWS s3 to upload user images when user wants to change your user image

--- Back-end/in-academy/in-academy/src/main/java/project/in_academy/controllers/UserController.java
+++ Back-end/in-academy/in-academy/src/main/java/project/in_academy/controllers/UserController.java
@@ -3,6 +3,7 @@ package project.in_academy.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import project.in_academy.model.User;
 import project.in_academy.service.UserService;
 
@@ -16,5 +17,10 @@ public class UserController {
     @GetMapping("/{email}")
     public ResponseEntity<User> getUserByEmail(@PathVariable String email){
         return ResponseEntity.ok().body(service.findUserByEmail(email));
+    }
+
+    @PostMapping("/image")
+    public ResponseEntity<String> changeUserImage(@RequestParam(value = "file") MultipartFile file, @RequestHeader("Authorization") String token){
+        return ResponseEntity.ok().body(service.changeUserImage(file, token));
     }
 }
--- Back-end/in-academy/in-academy/src/main/java/project/in_academy/service/UserService.java
+++ Back-end/in-academy/in-academy/src/main/java/project/in_academy/service/UserService.java
@@ -1,17 +1,51 @@
 package project.in_academy.service;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import project.in_academy.model.User;
 import project.in_academy.repository.UserRepository;
+
+import java.io.IOException;
 
 @Service
 public class UserService {
 
+    private final AmazonS3 amazonS3;
+
     @Autowired
     UserRepository repository;
 
+    @Autowired
+    TokenService tokenService;
+
+    public UserService(AmazonS3 amazonS3){
+        this.amazonS3 = amazonS3;
+    }
+
+    @Value("${cloud.aws.s3.bucket.name}")
+    private String bucketName;
+
     public User findUserByEmail(String email){
         return repository.findByEmail(email).orElseThrow(() -> new RuntimeException("Usuário não encontrado!"));
+    }
+
+    public String changeUserImage(MultipartFile file, String token) {
+        String email = tokenService.getSubject(token.replace("Bearer ", ""));
+        User user = repository.findByEmail(email).orElseThrow(() -> new RuntimeException("Usuário não encontrado!"));
+
+        String fileName = user.getUserId() + "." + file.getOriginalFilename().split("\\.")[1];
+
+        try {
+            amazonS3.putObject(new PutObjectRequest(bucketName, fileName, file.getInputStream(), null));
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to upload file", e);
+        }
+
+        return amazonS3.getUrl(bucketName, fileName).toString();
     }
 }
